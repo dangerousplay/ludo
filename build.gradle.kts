4e908597f3d432663302c1d125aa4c0a47e33523
Davi Henrique <gm.davi.gm@live.com>
2019-06-06 22:05:18 -0300

    initial commit.

---
+++ build.gradle.kts
@@ -0,0 +1,18 @@
+plugins {
+    java
+}
+
+group = "edu.lasalle"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testCompile("junit", "junit", "4.12")
+}
+
+configure<JavaPluginConvention> {
+    sourceCompatibility = JavaVersion.VERSION_11
+}
